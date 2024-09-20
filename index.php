c03867881f03f49bb09c3e94a4983948dc6af088
AinembabaziChristine022 <christineainembabazi656@gmail.com>
2024-09-20 13:29:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="AINEMBABAZI CHRISTINE";
+$DOB="27/12/2001";
+$Address="RUBANDA";
+$AYear=2024;
+$BYear=2001;
+$Age=$AYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+AYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
